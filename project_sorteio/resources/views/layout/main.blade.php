7f5e04d3c066f1a92e016ec3695163b4b2d11306
Rafael Victor Boscato <85459262+rs1297@users.noreply.github.com>
2021-09-13 16:06:51 -0300

    Criação da blade main + menu geral

---
+++ project_sorteio/resources/views/layout/main.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+
+
+    <title>Document</title>
+
+    <!--FONTES GOOGLE-->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Oldenburg&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300&display=swap');
+    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+
+    <!--IMPORT ICONS-->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+
+</head>
+<body>
+
+    <div class="corpo">
+
+        <div class="menu">
+
+            <div class="menucentral">
+                <a href="">Inicio</a>
+                <a href="">Sorteios</a>
+                <a href="">Criar Sorteio</a>
+            </div>    
+
+            <div class="menuentrar">
+                <ion-icon id="iconperfil" name="person-circle-outline"></ion-icon>
+            </div>
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+</body>
+</html>
