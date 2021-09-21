b586b8f6a0aa815f04f46499d09977d6837a7912
Rafael Victor Boscato <85459262+rs1297@users.noreply.github.com>
2021-09-21 19:33:54 -0300

    marcacao de numeros com js

--- project_sorteio/public/js/script.js
+++ project_sorteio/public/js/script.js
@@ -9,9 +9,18 @@ if (comprarfim == 0) {
 
         let numbert = document.getElementById(clicked_id)
 
-        numbert.style.background = 'red'
 
-        numex.push(clicked_id);
+        if (numex.indexOf(numbert.innerText) === -1) {
+            numbert.style.background = 'red'
+            numex.push(clicked_id);
+        } else {
+            numbert.style.background = 'rgb(10, 248, 10)'
+            let posi = numex.indexOf(clicked_id);
+            numex.splice(posi,1)
+        }
+
+
+        
 
         let quantescolha = document.querySelector('.confirmarnumeroquantidade')
 
@@ -25,5 +34,4 @@ if (comprarfim == 0) {
     function fim() {
         comprarfim = 1;
     }
-
 }
